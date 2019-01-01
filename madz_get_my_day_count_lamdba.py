5c78dc9ac7ad6e9dc8b6b18901e64cc280cc2219
richardxholloway <richardholloway@macbook-pro-8.home>
2019-01-01 10:26:38 +0000

    Get my day count working with Lambda and Dynamodb

--- madz_get_my_day_count_lamdba.py
+++ madz_get_my_day_count_lamdba.py
@@ -19,7 +19,7 @@ def getEndpoint():
 
 	endpoint_url = ''
 	#endpoint_url = "http://localhost:8000"
-	endpoint_url = "http://127.0.0.1:8000"
+	#endpoint_url = "http://127.0.0.1:8000"
 
 	return(endpoint_url)
 
