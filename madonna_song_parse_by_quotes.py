1b9ae946c53ad0604bce47710add247109c9a564
richardx14 <richard@dicecentre.org>
2019-02-22 10:37:00 +0000

    Fixed song parser.

--- madonna_song_parse_by_quotes.py
+++ madonna_song_parse_by_quotes.py
@@ -3,17 +3,14 @@ songArray = []
 
 with open('madonna_all_songs_wikipedia.txt') as file_object:
 #	contents = file_object.read()
+	
 	for line in file_object:
 		print(line)
+		
 		if line[0] == "\"":
 			songCounter = songCounter + 1 
-			#songArray.append(line)
 			songString = line.split("\"")[1]
 			songArray.append(songString)
-
-print(songCounter)
-
-print(songArray)
 
 
 outputFile = 'parsedSongList.txt'
@@ -23,10 +20,3 @@ with open(outputFile,'w') as file_object:
 		file_object.write(str("\"" + song + "\","))
 		file_object.write("\n")
 
-#print(contents)
-
-#myString="Hello there !bob@"
-#mySubString=myString[myString.find("!")+1:myString.find("@")]
-#print(mySubString)
-
-#songString = contents[contents.find("\"")+1:contents.find("\"")]
